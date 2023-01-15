63541da64de0a4c18420d0d4b6636a096e4518a4
Vangipurapulavanya <happy.vangipurapu@gmail.com>
2023-01-16 02:30:48 +0530

    Remove unused content

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
 import {modalsStore,cartStore} from './redux/Reducer';
 import { combineReducers, createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
@@ -14,17 +13,12 @@ const store = createStore(
   devToolsEnhancer()
 );
 
-
-
-// const store = configureStore({reducer: rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && });
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App/>
     </Provider>
-  
   </React.StrictMode>
 );
 
